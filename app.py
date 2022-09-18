91be17fd8a1fd252580c931385ee8f3689381664
FreeAaron <jason730lee@gmail.com>
2022-09-19 01:42:45 +0800

    改回單檔app.py架構

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from app import create_app
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
-app = create_app()
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='app/static/',
+            template_folder='app/templates/')
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
