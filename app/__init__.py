f32a22839a33273c9d14946ff278616dffd9c7fa
Weber Tsai <taida957789@gmail.com>
2022-09-18 20:39:08 +0800

    use flask static folder for static assets

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from app.route import hello_world, index
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
     app.add_url_rule('/', '/', index)
     return app
