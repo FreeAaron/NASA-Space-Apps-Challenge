60a53dfc61f077087acecb412d51edacd2811d48
FreeAaron <jason730lee@gmail.com>
2022-09-19 02:56:25 +0800

    拿掉多餘敘述

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__,
             static_url_path='', 
