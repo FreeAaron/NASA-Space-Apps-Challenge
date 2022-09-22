32f04071f2fc6c7101185b48afdf7c940f24e88b
FreeAaron <jason730lee@gmail.com>
2022-09-22 15:54:13 +0800

    Agency templates OK

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
+# app = Flask(__name__,
+#             static_url_path='', 
+#             static_folder='app/static/',
+#             template_folder='app/templates/')
+
 app = Flask(__name__,
-            static_url_path='', 
-            static_folder='app/static/',
-            template_folder='app/templates/')
+            static_url_path="/", 
+            static_folder='agency/static/',
+            template_folder='agency/templates/')
 
 @app.route('/')
 def index():
