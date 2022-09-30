593c43bfd7d81f910e30f88e025ac42eaa4077eb
FreeAaron <jason730lee@gmail.com>
2022-10-01 05:01:29 +0800

    arsha版

--- app.py
+++ app.py
@@ -5,15 +5,24 @@ from flask import Flask, render_template
 #             static_folder='app/static/',
 #             template_folder='app/templates/')
 
+# app = Flask(__name__,
+#             static_url_path="/", 
+#             static_folder='BS5/static/',
+#             template_folder='BS5/templates/')
+
 app = Flask(__name__,
-            static_url_path="/", 
-            static_folder='BS5/static/',
-            template_folder='BS5/templates/')
+            static_url_path="", 
+            static_folder='arsha/static/',
+            template_folder='arsha/templates/')
 
 @app.route('/')
 def index():
    print('Request for index page received')
    return render_template('index.html')
+
+@app.route("/details")
+def details():
+    return render_template("portfolio-details.html")
 
 if __name__ == "__main__":
     app.run()
