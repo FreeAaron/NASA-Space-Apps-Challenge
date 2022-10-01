e7c93e5504a85118920d048321bedf9ae4e0ff85
FreeAaron <jason730lee@gmail.com>
2022-10-02 04:07:09 +0800

    刪除多餘

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
 from flask import Flask, render_template
 
-# app = Flask(__name__,
-#             static_url_path='', 
-#             static_folder='app/static/',
-#             template_folder='app/templates/')
-
-# app = Flask(__name__,
-#             static_url_path="/", 
-#             static_folder='BS5/static/',
-#             template_folder='BS5/templates/')
-
 app = Flask(__name__,
             static_url_path='', 
             static_folder='Aaron/static/',
