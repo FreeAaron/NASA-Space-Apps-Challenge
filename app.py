ae71e70e3cf908bd41ab5d1ca8bcd1dcbc08c19a
FreeAaron <jason730lee@gmail.com>
2022-10-01 14:37:29 +0800

    修正logo連結

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ from flask import Flask, render_template
 #             template_folder='BS5/templates/')
 
 app = Flask(__name__,
-            static_url_path="", 
+            static_url_path='', 
             static_folder='Aaron/static/',
             template_folder='Aaron/templates/')
 
 @app.route('/')
+@app.route('/index')
 def index():
    print('Request for index page received')
    return render_template('index.html')
@@ -25,4 +26,4 @@ def details():
     return render_template("portfolio-details.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
