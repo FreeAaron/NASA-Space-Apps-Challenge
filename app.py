a4e4ea1659a68f63b5db0dc8e4f041ee70058cfd
FreeAaron <jason730lee@gmail.com>
2022-10-02 21:59:41 +0800

    details全部完成

--- app.py
+++ app.py
@@ -15,5 +15,29 @@ def index():
 def details():
     return render_template("portfolio-details.html")
 
+@app.route("/details2")
+def details2():
+    return render_template("App-details2.html")
+
+@app.route("/details3")
+def details3():
+    return render_template("App-details3.html")
+
+@app.route("/details4")
+def details4():
+    return render_template("App-details4.html")
+
+@app.route("/details5")
+def details5():
+    return render_template("Card-details5.html")
+
+@app.route("/details6")
+def details6():
+    return render_template("Card-details6.html")
+
+@app.route("/details7")
+def details7():
+    return render_template("Card-details7.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
